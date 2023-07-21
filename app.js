3094fb5349060a12fc13eb4252a94acd8adc86ed
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-22 00:06:30 +0800

    first try failed

---
+++ app.js
@@ -0,0 +1,9 @@
+// function getCard(){
+//     const num = ['1,2,3,4,5,6,7,8,9,10,J,Q,K'];
+//     const suit = ['clubs, spades, hearts, diamonds'];
+//     const pickNum = Math.floor(Math.random * 13) + 1;
+//     const pickSuit = Math.floor(Math.random * 4) + 1;
+//     const finalNum = num.indexOf[(pickNum)];
+//     const finalSuit = suit.indexOf[(pickSuit)];
+//     return `${finalNum} of ${finalSuit}`;
+// }
