c938509d2f17b65df478776127fb6fb070ac3ba6
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-22 00:45:17 +0800

    more variations

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// first idea
+
 // function getCard(){
 //     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 //     const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
@@ -8,13 +10,22 @@
 //     return `${finalNum} of ${finalSuit}`;
 // }
 
-function getCard(){
-    const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-    const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
-    const pickNum = Math.floor(Math.random() * num.length);
-    const pickSuit = Math.floor(Math.random() * suit.length);
-    return `${num[pickNum]} of ${suit[pickSuit]}`;
-}
+
+
+
+// gives String, attempt using first idea and ideas from vid
+
+// function getCard(){
+//     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+//     const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
+//     const pickNum = Math.floor(Math.random() * num.length);
+//     const pickSuit = Math.floor(Math.random() * suit.length);
+//     return `${num[pickNum]} of ${suit[pickSuit]}`;
+// }
+
+
+
+// video solution with modified names
 
 // function getCard(){
 //     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
@@ -24,3 +35,32 @@ function getCard(){
 //     return `${num[idx]} of ${suit[idy]}`;
 // }
 
+
+
+// return object
+
+// function getCard(){
+//     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+//     const idx = Math.floor(Math.random() * num.length);
+//     const finalNum = num[idx];
+//     const suit = ['clubs', 'spades', 'hearts', 'diamonds']
+//     const idy = Math.floor(Math.random() * suit.length);
+//     const finalSuit = suit[idy];
+//     return {value: finalNum, suit: finalSuit}
+// }
+
+
+
+
+// reusable function
+
+function pick(arr){
+    const ind = Math.floor(Math.random() * arr.length);
+    return arr[ind];
+}
+
+function getCard(){
+    const nums = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds']
+    return {number: pick(nums), suit: pick(suits)}
+}
