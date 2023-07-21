2c46584ad52944da32bdb9b60a217fa41071791c
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-22 00:27:30 +0800

    finished

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 // function getCard(){
-//     const num = ['1,2,3,4,5,6,7,8,9,10,J,Q,K'];
-//     const suit = ['clubs, spades, hearts, diamonds'];
+//     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+//     const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
 //     const pickNum = Math.floor(Math.random * 13) + 1;
 //     const pickSuit = Math.floor(Math.random * 4) + 1;
 //     const finalNum = num.indexOf[(pickNum)];
 //     const finalSuit = suit.indexOf[(pickSuit)];
 //     return `${finalNum} of ${finalSuit}`;
 // }
+
+function getCard(){
+    const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
+    const pickNum = Math.floor(Math.random() * num.length);
+    const pickSuit = Math.floor(Math.random() * suit.length);
+    return `${num[pickNum]} of ${suit[pickSuit]}`;
+}
+
+// function getCard(){
+//     const num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+//     const idx = Math.floor(Math.random() * num.length);
+//     const suit = ['clubs', 'spades', 'hearts', 'diamonds']
+//     const idy = Math.floor(Math.random() * suit.length);
+//     return `${num[idx]} of ${suit[idy]}`;
+// }
+
